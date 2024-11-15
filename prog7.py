7878da8e4793fe312ef30b651422b3fe6e19e1af
hafzanwazani <hafzanwazani03@gmail.com>
2024-11-15 23:04:20 +0800

    Add files via upload

---
+++ prog7.py
@@ -0,0 +1,9 @@
+# menghitung luas persegi panjang
+# nama: M Hafzan Wazani
+
+print("Menghitung luas persegi panjang")
+panjang = int(input("Masukan panjang: "))
+lebar = int(input("Masukan lebar: "))
+
+hasil = panjang * lebar
+print(f"Hasil dari {panjang} × {lebar} = {hasil}")
