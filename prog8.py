64c385055e2abc50e2e03fb1e42219646819cd0d
hafzanwazani <hafzanwazani03@gmail.com>
2024-11-29 10:14:19 +0800

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,28 @@
+x = 24
+y = 68
+#penjumlahan
+hasil = x + y
+print ("hasil dari x = y")
+#pengurangan
+hasil = x - y
+print ("nilai dari x - y = ", hasil)
+
+#pembagian
+hasil = x / y
+print ("hasil dari x / y = ", hasil)
+
+#perkalian
+hasil = x * y
+print ("hasil dari x * y =", hasil)
+
+#exponen (perpangkatan)
+hasil = x ** y
+print ("nilai dari x eksponen y =", hasil)
+
+#Modulus (sisa bagi)
+hasil = x % y
+print ("hasil dari x mod y =", hasil)
+
+#floor division
+hasil = x // y
+print ("hasil dari x FD y =", hasil)
