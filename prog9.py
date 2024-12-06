4a87d93aef5239af56870e9a35156360f2e281f6
hafzanwazani <hafzanwazani03@gmail.com>
2024-12-06 10:44:47 +0800

    Add files via upload

---
+++ prog9.py
@@ -0,0 +1,15 @@
+# Latihan Operasi Aritmatika
+
+nilai = float(input("Masukkan nilai suhu dalam Celsius: "))
+
+# Konversi ke Fahrenheit
+fahrenheit = (nilai * 9/5) + 32
+print("Nilai dari Fahrenheit = ", fahrenheit)
+
+# Konversi ke Kelvin
+kelvin = nilai + 273.15  # Gunakan titik bukan koma
+print("Nilai dari Kelvin = ", kelvin)
+
+# Konversi ke Reamur
+reamur = (nilai * 4/5)
+print("Nilai dari Reamur = ", reamur)
